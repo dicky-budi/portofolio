0de0dc0e335606c27fe1b84806d7754b5b778d39
Dicky Budianto <dicky.sugiono@nusantara.technology>
2024-12-11 11:17:14 +0700

    setup & apply prettier

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				primary: '#0f172a',
-				secondary: '#64748b',
-				accent: '#3b82f6',
-			},
-			fontFamily: {
-				sans: ['Inter var', 'sans-serif'],
-			},
-		},
-	},
-	plugins: [],
-	darkMode: 'class',
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0f172a",
+        secondary: "#64748b",
+        accent: "#3b82f6",
+      },
+      fontFamily: {
+        sans: ["Inter var", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+  darkMode: "class",
+};
